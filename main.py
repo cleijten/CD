fd5d2b8424dd0c07607e65959b7a341f2183a9e6
Celia Leijten <celialeijten@gmail.com>
2023-04-14 15:11:25 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# Import what we need from flask
+from flask import Flask
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route('/')
+def index():
+    return 'Hello, world! It works!'
+
