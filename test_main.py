d01cb2792ea5baa16b76775b851c850149f86659
Celia Leijten <celialeijten@gmail.com>
2023-04-21 17:21:18 +0200

    regenerated ssh keys

--- test_main.py
+++ test_main.py
@@ -3,5 +3,5 @@ from main import app
 def test_index():
     response = app.test_client().get('/')
     
-    assert response.data == b'Hello, world! It works !'
+    assert response.data == b'Hello, world! It works!'
     assert response.status_code == 200
